e8c69cbf754ae847b7a810281f0e86e2a90e210a
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2022-12-06 15:54:32 -0300

    Update valida_cpf.py

--- valida_cpf.py
+++ valida_cpf.py
@@ -3,7 +3,6 @@ Validador de CPF
 Usa o algoritmo da receita federal para verificar se um determinado CPF é válido
 """
 cpf = '746.824.890-70'.replace('.', '').replace('-', '')
-cpf = '89236550746'
 print('=-' * 40)
 if cpf.isnumeric() and len(cpf) == 11:
     cpf_nove = cpf[:9]
@@ -14,7 +13,6 @@ if cpf.isnumeric() and len(cpf) == 11:
         multi_1 -= 1
     resto_1 = (soma_1 * 10) % 11
     resto_1 = 0 if resto_1 > 9 else resto_1
-
     cpf_dez = cpf[:10]
     soma_2 = 0
     multi_2 = 11
