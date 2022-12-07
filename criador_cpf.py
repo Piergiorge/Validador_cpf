2220d2400476322d077c64c846557280ba9da7c7
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2022-12-06 21:17:07 -0300

    Update criador_cpf.py

--- criador_cpf.py
+++ criador_cpf.py
@@ -12,34 +12,24 @@ def criar_cpf():
     return string_cpf
 
 
-def gerador_cpf(cpf, contador):
+def gerador_cpf(temp_cpf, contador):
     """
     Função que cria o décimo e décimo primeiro dígito do CPF
-    :param cpf:
-    :param contador:
-    :return:
     """
     soma = 0
-    for num in cpf:
+    for num in temp_cpf:
         assert isinstance(num, object)
         soma += contador * int(num)
         contador -= 1
-    return soma
+    resto = (soma * 10) % 11
+    return resto if resto <= 9 else 0
 
 
 cpf_gerado = criar_cpf()
 
-soma_1 = gerador_cpf(cpf_gerado, 10)
-
-resto_1: int = (soma_1 * 10) % 11
-resto_1 = 0 if resto_1 > 9 else resto_1
-
+resto_1 = gerador_cpf(cpf_gerado, 10)
 cpf_dez = cpf_gerado + str(resto_1)
 
-soma_2 = gerador_cpf(cpf_dez, 11)
-
-resto_2: int = (soma_2 * 10) % 11
-resto_2 = 0 if resto_2 > 9 else resto_2
+resto_2 = gerador_cpf(cpf_dez, 11)
 cpf_novo = cpf_gerado + str(resto_1) + str(resto_2)
-
 print(f'CPF gerado: {cpf_novo[0:3]}.{cpf_novo[3:6]}.{cpf_novo[6:9]}-{cpf_novo[9:]}')
